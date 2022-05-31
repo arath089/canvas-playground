98cc0e55555284a3a7d49bc2b61d3b0014031aa7
Archit Rathi <archi@thebeyondgroup.la>
2022-05-31 08:56:23 -0700

    Creating Circle Object and updating its values
    In this commit, which is a big one, lol, we create a Circle Object which will get its values from the
    circleArray and we update those values using the update function.
    We also randomize the values being pushed.

--- canvas.js
+++ canvas.js
@@ -15,26 +15,57 @@ var c = canvas.getContext('2d');
 //     c.stroke();
 // }
 
-var radius = 30;
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = (Math.random() - 0.5) * 8;
-var dy = (Math.random() - 0.5) * 8;
+
+
+// Circle Object
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function(){       //Anonymous Draw function for drawing circles
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.stroke();
+    }
+
+    this.update= function(){    //Update function to update the values
+        if( this.x + this.radius > innerWidth || this.x - this.radius < 0 ){
+            this.dx = -this.dx;
+        }
+        if( this.y + this.radius > innerHeight || this.y - this.radius < 0 ){
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+
+        this.draw();
+    }
+}
+
+
+var circleArray = [];
+
+for( var i= 0; i< 100; i++){
+    var radius = 30;
+    var x = Math.random() * innerWidth;
+    var y = Math.random() * innerHeight;
+    var dx = (Math.random() - 0.5) * 5;
+    var dy = (Math.random() - 0.5) * 5;
+
+    circleArray.push(new Circle(x, y, dx, dy, radius));                //circleArray is going to push a new circle each time this for loop is ran through
+}
+
 
 function animate(){             //creating an animate loop
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
-    c.beginPath();
-    c.arc(x, y, radius, 0, Math.PI * 2, false);
-    c.stroke();
-    if( x + radius > innerWidth || x - radius < 0 ){
-        dx = -dx;
+
+    for( var i=0; i< circleArray.length; i++){
+        circleArray[i].update();
     }
-    if( y + radius > innerHeight || y - radius < 0 ){
-        dy = -dy;
-    }
-    x += dx;
-    y += dy;
 }
 
 animate();
