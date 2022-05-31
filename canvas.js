152ccf10b5c4c84c0b971c14e4f57b9330ee2c7c
Archit Rathi <archi@thebeyondgroup.la>
2022-05-31 08:06:25 -0700

    Animating 1 circle to bounce
    In this commit, we animate just one circle and we animate it to
    bounce off the walls of the browser window.

--- canvas.js
+++ canvas.js
@@ -7,10 +7,29 @@ var c = canvas.getContext('2d');
 
 // Multiple Circles at same location
 
-for( var i=0; i<100; i++){
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
+// for( var i=0; i<100; i++){
+//     var x = Math.random() * window.innerWidth;
+//     var y = Math.random() * window.innerHeight;
+//     c.beginPath();
+//     c.arc(x, y, 30, 0, Math.PI * 2, false);
+//     c.stroke();
+// }
+
+var radius = 30;
+var x = 200;
+var dx = 3;
+var y = 200;
+
+function animate(){             //creating an animate loop
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
     c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
+    c.arc(x, 200, radius, 0, Math.PI * 2, false);
     c.stroke();
+    if( x + radius > innerWidth || x - radius < 0 ){
+        dx = -dx;
+    }
+    x += dx;
 }
+
+animate();
