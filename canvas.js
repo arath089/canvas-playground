87fdfcea6a394e7e8646220be092bafc84ff0aa1
Archit Rathi <archi@thebeyondgroup.la>
2022-05-31 07:43:39 -0700

    Basic building blocks

---
+++ canvas.js
@@ -0,0 +1,15 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+// Rectangle
+c.fillRect(100, 100, 200, 200);
+c.fillRect(300, 300, 200, 200);
+c.fillRect(600, 400, 200, 200);
+
+// Circle
+c.arc(800, 300, 30, 0, Math.PI * 2, false);
+c.stroke();
